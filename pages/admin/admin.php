96ff63a473048e54b3c1da26b6199d76b6937916
Daniela Giraldo Quintero <danielagiraldo@nusoft.com.co>
2020-04-24 00:16:57 -0500

    review

--- pages/admin/admin.php
+++ pages/admin/admin.php
@@ -1,22 +1,30 @@
 <?php
 
-// if($_REQUEST['script'] == 'database'){
-//     $script = 'create_database.sql';
-// }if($_REQUEST['script'] == 'table'){
-//     $script = 'create_table.sql';
-// }else{
-//     echo "El script no fue encontrado";
-// }
+function exec_script($type = ''){
+    
+    if(isset($type) && $type == 'database'){
+        $script = 'create_database.sql';
+    }if(isset($type) && $type == 'table'){
+        $script = 'create_table.sql';
+    }else{
+        $result = "El script no fue encontrado";
+    }
 
-$command = "mysql -uroot -p12345678 -h localhost -e 'CREATE DATABASE IF NOT EXISTS `bdunad29`'";
+    $command = "mysql -uroot -p12345678 -h localhost --force < scripts/". $script;
 
-$result = shell_exec($command);
-print_r($result);
+    $result = shell_exec($command);
+    print_r($result);
 
-// if($result == true){
-//     echo 'Script ejecutado correctamente';
-// }else{
-//     echo 'Script incorrecto';
-// }
+    return $result;
+}
+
 
 ?>
+<input type="hidden" id="exec" value="" onchange="exec();">
+<!-- <label id="result_exec"></label> -->
+<script>
+    function exec(){
+        var type = $('#exec').attr('value');
+        <?= exec_script('database'); ?>
+    }
+</script>
